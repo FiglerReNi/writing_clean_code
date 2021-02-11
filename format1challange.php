b58a972f664113f5a5d5c876cd1fe3c2208a9c04
FiglerReni <b1284ca6ae8e891a365c46f441db1027>
2021-02-11 14:42:30 +0100

    forras

---
+++ format1challange.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @author Figler Renáta
+ * @version v.1.0 (2021/02/11)
+ * @copyright Copyright (c.) 2021, Figler Renáta
+ */
+
+include 'path.php';
+include 'makedirs.php';
+
+class DiskStorage
+{
+
+    /**
+     * @var String
+     */
+    private string $directoryName;
+
+    /**
+     * DiskStorage constructor.
+     * @param String $directoryName
+     */
+    public function __construct(string $directoryName)
+    {
+        $this->directoryName = $directoryName;
+    }
+
+    /**
+     * @return String
+     */
+    public function getDirectoryName(): string
+    {
+        return $this->directoryName;
+    }
+
+    private function createDirectory(): void
+    {
+        if (!file_exists($this->directoryName)) {
+            mkdir($this->directoryName);
+        }
+    }
+
+    /**
+     * @param string $fileName
+     * @param string $content
+     */
+    public function insertFile(string $fileName, string $content)
+    {
+        $this->createDirectory();
+        $file = fopen($this->directoryName . $fileName, "w");
+        fwrite($file, $content);
+        fclose($file);
+        # Todo: Add proper error handling
+    }
+}
+
+$logStorage = new DiskStorage('/logs/');
+$logStorage->insertFile('text.txt', 'Test');
+
+
